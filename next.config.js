4ad3118c7687c89525d93ad201b6029f554297a2
tnk050 <hexangleff@gmail.com>
2022-11-15 15:56:27 +0900

    add path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath: process.env.GITHUB_ACTIONS && '/medal_control_sample',
+  trailingSlash: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
